0a6f4d5fb07e01ebb1dd42b7ed81e4abb6db228c
Vivek Kumar Dwivedi <vivekdwivedi498@gmail.com>
2026-03-14 10:33:46 +0530

    remove repetitive connection check and add a centralize connection mangement from all repo methods

--- src/repositories/cache.repository.ts
+++ src/repositories/cache.repository.ts
@@ -1,38 +1,35 @@
 import { serverConfig } from "../config";
 import { redisClient } from "../config/redis";
 export class CacheRepository{
-
-    async getNextId():Promise<number>{
-        const key = serverConfig.REDIS_COUNTER_KEY;
+    
+    private async ensureConnected(){
         if(!redisClient.isOpen){
             await redisClient.connect();
         }
+    }
+    async getNextId():Promise<number>{
+        const key = serverConfig.REDIS_COUNTER_KEY;
+        await this.ensureConnected();
         const result = await redisClient.incr(key);
         return result;
     }
 
     async setUrlMapping(shortUrl:string, originalUrl:string){
         const key = `url.${shortUrl}`;
-        if(!redisClient.isOpen){
-            await redisClient.connect();
-        }
+        await this.ensureConnected();
         await redisClient.set(key, originalUrl,{EX:86400});
     }
 
     async getUrlMapping(shortUrl:string):Promise<string|null>{
         const key = `url.${shortUrl}`;
-        if(!redisClient.isOpen){
-            await redisClient.connect();
-        }
+        await this.ensureConnected();
         const result = await redisClient.get(key);
         return result;
     }
 
     async deleteUrlMapping(shortUrl:string):Promise<void>{
         const key = `url.${shortUrl}`;
-        if(!redisClient.isOpen){
-            await redisClient.connect();
-        }
+        await this.ensureConnected();
         await redisClient.del(key);
     }
 }
